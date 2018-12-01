06f8cfdd71bd46849978f2fe9dc38a1aa5dc859d
Lawrence Woodman <lwoodman@vlifesystems.com>
2018-12-01 13:18:17 +0000

    Import project into repo

---
+++ 2018/day1.tcl
@@ -0,0 +1,38 @@
+# Copyright (C) 2018 Lawrence Woodman <lwoodman@vlifesystems.com>
+# Licensed under an MIT licence.  Please see LICENCE.md for details.
+
+set fp [open "day1.input" r]
+set input [read $fp]
+close $fp
+set input [split $input "\n"]
+
+proc part1 {input} {
+  set sum 0
+  foreach line $input {
+    if {$line != ""} {
+      set sum [expr {$sum + $line}]
+      set freqs($sum) 1
+    }
+  }
+  return $sum
+}
+
+proc part2 {freqs startFreq input} {
+  dict set freqs $startFreq 1
+  set sum $startFreq
+  foreach line $input {
+    if {$line != ""} {
+      set sum [expr {$sum + $line}]
+      if {[dict exists $freqs $sum]} {
+        return $sum
+      } else {
+        dict set freqs $sum 1
+      }
+    }
+  }
+  tailcall part2 $freqs $sum $input
+}
+
+puts "part1: [part1 $input]"
+puts "part2: [part2 {} 0 $input]"
+
---
+++ README.md
@@ -0,0 +1,12 @@
+Advent of Code
+==============
+
+Entries for the [Advent of Code](https://adventofcode.com) challenge.
+
+
+Licence
+-------
+Copyright (C) 2018 Lawrence Woodman <lwoodman@vlifesystems.com>
+
+This software is licensed under an MIT Licence.  Please see the file, [LICENCE.md](https://github.com/lawrencewoodman/adventofcode/blob/master/LICENCE.md), for details.
+
